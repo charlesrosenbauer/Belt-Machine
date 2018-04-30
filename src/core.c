89c337b8b05e5e3f89adee941915d4aba7265691
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-04-30 23:54:43 +0000

    Added to instruction printing

--- src/core.c
+++ src/core.c
@@ -9,10 +9,36 @@
 
 
 
+// Temporary techniques
+inline void printOp_2Par(uint16_t ins){
+  int a = ins & 0x1F;
+  int b = (ins >> 5) & 0x1F;
+  int op= ins >> 10;
+  printf("  %i b%i b%i\n", op, a, b);
+}
+
+inline void printOp_3Par(uint16_t ins){
+  int a = ins & 0x1F;
+  int b = (ins >>  5) & 0x1F;
+  int c = (ins >> 10) & 0x1F;
+  int op= ins >> 15;
+  printf("  %i b%i b%i b%i\n", op, a, b, c);
+}
+
+
+
+
+
+
+
+
+
 
 void printDecode(uint16_t* ip){
   if(ip[0] | ip[1]){ // If not NOP
     uint32_t header = (((uint32_t)ip[0]) << 16) | ip[1];
+
+    printf("%x\n\n", header);
 
     int index = 2;
     for(int slot = 0; slot < 32; slot++){
@@ -22,6 +48,7 @@ void printDecode(uint16_t* ip){
         // Read Phase
         if((1 << slot) & header){
           // Decode Instruction
+          printOp_2Par(ip[index]);
           index++;
         }
       }else if(slot < 16){
@@ -29,6 +56,7 @@ void printDecode(uint16_t* ip){
         // Exec Phase
         if((1 << slot) & header){
           // Decode Instruction
+          printOp_2Par(ip[index]);
           index++;
         }
       }else if(slot < 24){
@@ -36,6 +64,7 @@ void printDecode(uint16_t* ip){
         // Call Phase
         if((1 << slot) & header){
           // Decode Instruction
+          printOp_2Par(ip[index]);
           index++;
         }
       }else if(slot < 28){
@@ -43,6 +72,7 @@ void printDecode(uint16_t* ip){
         // Pick Phase
         if((1 << slot) & header){
           // Decode Instruction
+          printOp_3Par(ip[index]);
           index++;
         }
       }else{
@@ -50,6 +80,7 @@ void printDecode(uint16_t* ip){
         // Write Phase
         if((1 << slot) & header){
           // Decode Instruction
+          printOp_2Par(ip[index]);
           index++;
         }
       }
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "stdio.h"
 #include "belt.h"
+#include "memory.h"
+#include "core.h"
 
 
 
@@ -11,5 +13,14 @@
 
 
 int main(){
-  printf("Hello World!");
+  RAM ram = initRAM(1048576);
+
+  /*
+    I have to be careful writing programs this way. The translation to uint16_t
+    doesn't play nicely with endianess, and bytes tend to get shuffled around in
+    the decode function. I need to find a better way to do this.
+  */
+  char* program = "\x00\x01\x00\x00\xFF\xFF";
+
+  printDecode((uint16_t*)program);
 }
