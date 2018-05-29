f03685efb94e6a11e1cbaa1891b685fc438dd789
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-05-29 23:38:07 +0000

    Changing to a grid-based delay system, because KISS.

--- src/core.h
+++ src/core.h
@@ -26,6 +26,53 @@ typedef struct{
 
 
 
+typedef enum {
+  DSM_REGISTER = 0;
+  DSM_LOAD     = 1;
+} DELAYSLOTMODE;
+
+typedef struct{
+  REGISTER r;
+  DELAYSLOTMODE mode;
+}DELAYSLOT;
+
+
+
+
+
+
+
+
+
+
+typedef struct{
+  DELAYSLOT slots [24][33];
+  uint32_t  activeMask[33];
+  int       timeIndex;
+  /*
+     0- 8 are  1 cycle READ slots
+     9-16 are  1 cycle ALU  slots
+    17-18 are  3 cycle ALU  slots
+    19-20 are 10 cycle ALU  slots
+    21-24 are variable delay LOAD slots
+
+    CALL and PICK stages aren't handled via delay, as they have no virtual delay
+    The WRITE stage produces no belt outputs, so nothing needed there.
+
+    activeMask features a bit for each slot. If a slot's corresponding bit is
+    zero, it will NOT be put on the belt.
+  */
+}DELAYGRID;
+
+
+
+
+
+
+
+
+
+
 typedef struct{
   BELT belt;
 
